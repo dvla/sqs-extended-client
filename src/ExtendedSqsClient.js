1b520c6d904b99c3da568bc57cc9a930d8d924f1
Plitzer <57969413+Plitzer@users.noreply.github.com>
2021-10-26 12:27:15 +1100

    Update ExtendedSqsClient.js

--- src/ExtendedSqsClient.js
+++ src/ExtendedSqsClient.js
@@ -217,7 +217,7 @@ class ExtendedSqsClient {
 
     middleware() {
         return {
-            before: ({ event }, next) => {
+            before: ({ event }) => {
                 return Promise.all(
                     event.Records.map(async (record) => {
                         const { bucketName, s3MessageKey } = getS3MessageKeyAndBucket(record);
@@ -240,8 +240,6 @@ class ExtendedSqsClient {
                         }
                     })
                 )
-                    .then(() => next())
-                    .catch((err) => next(err));
             },
         };
     }
